e09f11194d8d8dcc9ddfd99fd9abfc19f317f3e8
Hideko <hideko332200@gmail.com>
2024-06-25 17:27:00 +0900

    note add

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,11 @@
+import Image from "../assets/logo.png"
+
 export default function Logo() {
     return (
         <>
             <img
                 style={{ width: 50, filter: `invert(1)` }}
-                src="https://media.discordapp.net/attachments/1196455391085744179/1254045006289965056/image-removebg-preview.png?ex=66780fec&is=6676be6c&hm=249aeb3234667a5964cc4fcf9d74b9781ace5104180527cb5a2af5869cbc9ef3&=&format=webp&quality=lossless"
+                src={Image}
                 alt=""
             />
             <p style={{ marginLeft: 5 ,fontSize: 20, fontWeight: 800 }}>Kuroneko Network</p>
